c3a656ffe13ae2fed0f80ebdfaaeb7686ea00e5b
Ubuntu 12.04 <neel@ubuntu.(none)>
2013-03-03 23:02:42 -0600

    INitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wcsf::Application.config.secret_token = 'c9d887c4dd9bd93fffbd2b9884b1c30748844c972f6cf5a513853d7ef35e4c24f93c77e910819dbd1b744534bed2b26cd7e14acefd565e2486357f0688c01d07'
