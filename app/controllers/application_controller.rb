27c29e2ff3270421b98a162a33f5fe856f694700
Ubuntu 12.04 <neel@ubuntu.(none)>
2013-05-23 20:59:30 -0500

    Changed login redirection to MyBadges page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 	
 	def after_sign_in_path_for(resource)
 		if current_user.is_student?
-			return summary_path
+			return mybadges_path
 		end
 
 		return root_path
