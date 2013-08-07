fcd4acce76744d183a44980f135bf024ea9ce1d9
Ubuntu 12.04 <neel@ubuntu.(none)>
2013-08-07 00:08:24 -0500

    Updated UI for Extracurricular page

--- app/assets/javascripts/pages/header.js
+++ app/assets/javascripts/pages/header.js
@@ -5,26 +5,6 @@ var Header = new function() {
 		// Set the selected page in the navigation menu
 		var navItem = ".nav > li[page='" + page + "']";
 		$(".nav > li").removeClass("selected");
-		$(navItem).addClass("selected");
-
-		$(document).ready(function() {
-			// Adjust leftpanel size			
-			$(window).resize(function() {
-				self.setPageContainerHeight();
-			});
-		});		
+		$(navItem).addClass("selected");		
 	};	
-
-	self.setPageContainerHeight = function()
-	{
-		if (window.innerHeight > $('.pageContainer').height() + $('#mynav').height())
-		{
-			var height = window.innerHeight - $('#mynav').height();
-			$('.pageContainer').height(height);
-		}
-		else
-		{
-			$('.pageContainer').height('100%');
-		}
-	}
 }
