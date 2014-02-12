a6c74912f14c3f815d46aad5207a0afe34ca9a9b
Ubuntu 12.04 <neel@ubuntu.(none)>
2014-02-11 23:16:13 -0600

    Finished basic wiring up to backend of student dashboard page. All category highlights are showing with badges

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -17,25 +17,66 @@ class StudentRepository
 		student = Student.new
 	  	student.first_name = user.first_name
 	  	student.last_name = user.last_name
+	  	#student.points_earned = student.badges.points_earned
+	  	student.badges = BadgeFactory.new(user).GetBadges(semester).map{|b| BadgeViewModel.new(b) }
 
 	  	student.Academics = Academics.new
-	  	student.Academics.semester_gpa = AcademicsRepository.new(user).GetSemesterGpa(semester)
-	  	student.Academics.badges = BadgeFactory.new(user).GetBadges(semester, "Academics")
-                                             .map{|b| BadgeViewModel.new(b) }
+	  	student.Academics.semester_gpa = AcademicsRepository.new(user).GetSemesterGpa(semester)	  	
+	  	
+	  	student.Extracurriculars = Extracurriculars.new
+	  	student.Extracurriculars.total_activities = user.user_activities.where(:semester => semester).length	  	
+
+     	student.Service = Service.new
+	  	student.Service.total_hours = user.user_services.where(:semester => semester).sum(:hours)	  	
+
+		student.PDU = PDU.new
+	  	student.PDU.total_pdus = user.user_pdus.where(:semester => semester).length	  	
+
+		student.Testing = Testing.new
+	  	student.Testing.percent_complete = (PracticeTestRepository.new.GetPercentCompletedInfo(user.id)[:percentComplete_f] * 100).to_i	  	
 
 	  	return student
 	end
 end
 
 class Student
-	attr_accessor :first_name, :last_name, :Academics
+	attr_accessor :first_name, :last_name, :points_earned, :badges,
+				  :Academics, :Extracurriculars, :Service, :PDU, :Testing
 
 	def initialize
 	end
 end
 
 class Academics
-	attr_accessor :semester_gpa, :badges
+	attr_accessor :semester_gpa
+
+	def initialize
+	end
+end
+
+class Extracurriculars
+	attr_accessor :total_activities
+
+	def initialize
+	end
+end
+
+class Service
+	attr_accessor :total_hours
+
+	def initialize
+	end
+end
+
+class PDU
+	attr_accessor :total_pdus
+
+	def initialize
+	end
+end
+
+class Testing
+	attr_accessor :percent_complete
 
 	def initialize
 	end
